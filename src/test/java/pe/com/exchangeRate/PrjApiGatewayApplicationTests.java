4904794bfb23004ae86a39e4ff1687eedf648d26
unknown <vmillamora@gmail.com>
2023-11-28 00:32:26 -0500

    comitenado proj-api-gateway

---
+++ src/test/java/pe/com/exchangeRate/PrjApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.exchangeRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
